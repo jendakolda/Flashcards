eaf415f46a1e49af7cd3a6ae795d4c624d7bdc62
Já <email@jendakolda.cz>
2021-11-03 21:51:14 +0100

    Stage 4 incomplete

--- Flashcards/task/flashcards/flashcards.py
+++ Flashcards/task/flashcards/flashcards.py
@@ -1,9 +1,17 @@
+class TermDuplicityError(Exception):
+    pass
+
+
+class DefDuplicityError(Exception):
+    pass
+
+
 class Card:
     def __init__(self, front, back):
         self.front = front
         self.back = back
 
-    def main(self):
+    def card_check(self):
         answer = input(f'Print the definition of "{self.front}":\n')
         print('Correct' if self.back == answer else f'Wrong. The right answer is "{self.back}"')
 
@@ -12,8 +20,12 @@ if __name__ == '__main__':
     deck = {}
     card_count = int(input('Input the number of cards:\n'))
     for i in range(card_count):
-        deck[str(i)] = Card(input(f'The term for card #{i + 1}:\n'),
-                            input(f'The definition for card #{i + 1}:\n'))
+        try:
+            term = input(f'Term for card #{i + 1}:\n')
+            definition = input(f'The definition for card #{i + 1}:\n')
+            deck[str(i)] = Card(term, definition)
+        except TermDuplicityError:
+        except DefDuplicityError:
 
     for value in deck.values():
-        value.main()
+        value.card_check()
