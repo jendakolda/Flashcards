04c9e0c825bfaa29d404c1c83f4013eb7c1379e6
Já <email@jendakolda.cz>
2021-11-08 21:06:15 +0100

    Stage 5 incomplete 1

--- Flashcards/task/flashcards/flashcards.py
+++ Flashcards/task/flashcards/flashcards.py
@@ -12,34 +12,14 @@ class DefDuplicityError(Exception):
 
 class Card:
     deck = {}
-    commands = {}
+
     def __init__(self, front, back):
         self.front = front
         self.back = back
 
-    def card_check(self):
-        answer = input(f'Print the definition of "{self.front}":\n')
-        if answer == self.back:
-            return 'Correct'
-        elif answer != self.back and answer in [Card.deck[j].back for j in Card.deck.keys()]:
-            return f'Wrong.The right answer is "{self.back}", but your definition is correct ' \
-                   f'for "{Card.get_correct_term(answer)}".'
-        else:
-            return f'Wrong. The right answer is "{self.back}"'
-
     @staticmethod
-    def get_correct_term(unknown_definition):
-        for j in Card.deck.keys():
-            if unknown_definition == Card.deck[j].back:
-                return Card.deck[j].front
-        return 'Unknown'
-
-
-if __name__ == '__main__':
-    # card_count = int(input('Input the number of cards:\n'))
-    action = input('Input the action (add, remove, import, export, ask, exit):\n')
-    for i in range(card_count):
-        print(f'The term for card #{i + 1}:')
+    def add_card():
+        print('The Card:\n')
         while True:
             term = input()
             try:
@@ -49,7 +29,7 @@ if __name__ == '__main__':
             except TermDuplicityError as termerr:
                 print(termerr)
 
-        print(f'The definition for card #{i + 1}:')
+        print(f'The definition of the card:\n')
         while True:
             definition = input()
             try:
@@ -58,10 +38,49 @@ if __name__ == '__main__':
                 break
             except DefDuplicityError as deferr:
                 print(deferr)
+        Card.deck[term] = definition
 
-        Card.deck[str(i)] = Card(term, definition)
+    def remove_card(self):
+        pass
 
-    for value in Card.deck.values():
-        print(value.card_check())
+    def import_card(self):
+        pass
+
+    def export_card(self):
+        pass
+
+    def ask_card(self):
+        for _ in range(int(input('How many times to ask:\n'))):
+            answer = input(f'Print the definition of "{self.front}":\n')
+            if answer == self.back:
+                print('Correct')
+            elif answer != self.back and answer in [Card.deck[j].back for j in Card.deck.keys()]:
+                print(f'Wrong.The right answer is "{self.back}", but your definition is correct ' \
+                      f'for "{Card.get_correct_term(answer)}".')
+            else:
+                print(f'Wrong. The right answer is "{self.back}"')
+
+    @staticmethod
+    def exit_program():
+        exit()
+
+    @staticmethod
+    def get_correct_term(unknown_definition):
+        for j in Card.deck.keys():
+            if unknown_definition == Card.deck[j].back:
+                return Card.deck[j].front
+        return 'Unknown'
+
+    commands = {'add': add_card, 'remove': remove_card, 'import': import_card, 'export': export_card,
+                'ask': ask_card, 'exit': exit_program, }
+
+    @staticmethod
+    def card_main(name):
+        Card.commands.get(name, lambda: 'Invalid')()
 
 
+if __name__ == '__main__':
+    # card_count = int(input('Input the number of cards:\n'))
+    while True:
+        action = input('Input the action (add, remove, import, export, ask, exit):\n')
+        Card.commands[action]()
